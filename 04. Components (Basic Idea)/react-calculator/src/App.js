def38d146238f540f7bed26024f16e8b1750fc90
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-03-03 18:05:35 +0200

    Calculator business login

--- 04. Components (Basic Idea)/react-calculator/src/App.js
+++ 04. Components (Basic Idea)/react-calculator/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState("");
+
+  const handlerClick = (e) => {
+    setResult(result.concat(e.target.id));
+  };
+
+  function clear() {
+    setResult("");
+  }
+
+  const deleteElement = () => {
+    setResult
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <input type="text" value={result} disabled/>
+
+      <div className="buttons">
+
+      </div>
     </div>
   );
 }
