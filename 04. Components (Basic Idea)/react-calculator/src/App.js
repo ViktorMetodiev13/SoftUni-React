804599fe0c81e7f91afa6072c2b52c9c566771e3
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-03-03 18:20:44 +0200

    html added

--- 04. Components (Basic Idea)/react-calculator/src/App.js
+++ 04. Components (Basic Idea)/react-calculator/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
   const [result, setResult] = useState("");
@@ -13,16 +13,47 @@ function App() {
   }
 
   const deleteElement = () => {
-    setResult
-  }
+    setResult(result.slice(0, -1));
+  };
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult("Invalid");
+    }
+  };
 
   return (
     <div className="calculator">
-      <input type="text" value={result} disabled/>
+      <input type="text" value={result} disabled />
 
       <div className="buttons">
+        <button className="operator" onClick={clear}>AC</button>
+        <button className="operator" onClick={deleteElement}>DE</button>
+        <button id="." className="operator" onClick={handlerClick}>.</button>
+        <button id="/" className="operator" onClick={handlerClick}>/</button>
 
+        <button id="7" className="number" onClick={handlerClick}>7</button>
+        <button id="8" className="number" onClick={handlerClick}>8</button>
+        <button id="9" className="number" onClick={handlerClick}>9</button>
+        <button id="*" className="operator" onClick={handlerClick}>X</button>
+
+        <button id="4" className="number" onClick={handlerClick}>7</button>
+        <button id="5" className="number" onClick={handlerClick}>8</button>
+        <button id="6" className="number" onClick={handlerClick}>9</button>
+        <button id="-" className="operator" onClick={handlerClick}>X</button>
+
+        <button id="1" className="number" onClick={handlerClick}>1</button>
+        <button id="2" className="number" onClick={handlerClick}>2</button>
+        <button id="3" className="number" onClick={handlerClick}>3</button>
+        <button id="+" className="operator" onClick={handlerClick}>+</button>
+
+        <button id="00" className="number" onClick={handlerClick}>00</button>
+        <button id="0" className="number" onClick={handlerClick}>0</button>
+        <button id="=" className="operator col-span-2" onClick={calculate}>=</button>
       </div>
+
     </div>
   );
 }
