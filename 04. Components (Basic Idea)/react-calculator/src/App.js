9631dabc2af52cb4bbf67f34f3879f3b82ba021b
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-03-04 19:59:28 +0200

    Testing whether node works or not

--- 04. Components (Basic Idea)/react-calculator/src/App.js
+++ 04. Components (Basic Idea)/react-calculator/src/App.js
@@ -6,7 +6,6 @@ function App() {
 
   const handlerClick = (e) => {
     setResult(result.concat(e.target.id));
-    console.log((result.concat(e.target.id)));
   };
 
   function clear() {
@@ -30,29 +29,67 @@ function App() {
       <input type="text" value={result} disabled />
 
       <div className="buttons">
-        <button className="operator" onClick={clear}>AC</button>
-        <button className="operator" onClick={deleteElement}>DE</button>
-        <button id="." className="operator" onClick={handlerClick}>.</button>
-        <button id="/" className="operator" onClick={handlerClick}>/</button>
+        <button className="operator" onClick={clear}>
+          AC
+        </button>
+        <button className="operator" onClick={deleteElement}>
+          DE
+        </button>
+        <button id="." className="operator" onClick={handlerClick}>
+          .
+        </button>
+        <button id="/" className="operator" onClick={handlerClick}>
+          /
+        </button>
 
-        <button id="7" className="number" onClick={handlerClick}>7</button>
-        <button id="8" className="number" onClick={handlerClick}>8</button>
-        <button id="9" className="number" onClick={handlerClick}>9</button>
-        <button id="*" className="operator" onClick={handlerClick}>X</button>
+        <button id="7" className="number" onClick={handlerClick}>
+          7
+        </button>
+        <button id="8" className="number" onClick={handlerClick}>
+          8
+        </button>
+        <button id="9" className="number" onClick={handlerClick}>
+          9
+        </button>
+        <button id="*" className="operator" onClick={handlerClick}>
+          X
+        </button>
 
-        <button id="4" className="number" onClick={handlerClick}>4</button>
-        <button id="5" className="number" onClick={handlerClick}>5</button>
-        <button id="6" className="number" onClick={handlerClick}>6</button>
-        <button id="-" className="operator" onClick={handlerClick}>-</button>
+        <button id="4" className="number" onClick={handlerClick}>
+          4
+        </button>
+        <button id="5" className="number" onClick={handlerClick}>
+          5
+        </button>
+        <button id="6" className="number" onClick={handlerClick}>
+          6
+        </button>
+        <button id="-" className="operator" onClick={handlerClick}>
+          -
+        </button>
 
-        <button id="1" className="number" onClick={handlerClick}>1</button>
-        <button id="2" className="number" onClick={handlerClick}>2</button>
-        <button id="3" className="number" onClick={handlerClick}>3</button>
-        <button id="+" className="operator" onClick={handlerClick}>+</button>
+        <button id="1" className="number" onClick={handlerClick}>
+          1
+        </button>
+        <button id="2" className="number" onClick={handlerClick}>
+          2
+        </button>
+        <button id="3" className="number" onClick={handlerClick}>
+          3
+        </button>
+        <button id="+" className="operator" onClick={handlerClick}>
+          +
+        </button>
 
-        <button id="00" className="number" onClick={handlerClick}>00</button>
-        <button id="0" className="number" onClick={handlerClick}>0</button>
-        <button id="=" className="operator col-span-2" onClick={calculate}>=</button>
+        <button id="00" className="number" onClick={handlerClick}>
+          00
+        </button>
+        <button id="0" className="number" onClick={handlerClick}>
+          0
+        </button>
+        <button id="=" className="operator col-span-2" onClick={calculate}>
+          =
+        </button>
       </div>
     </div>
   );
