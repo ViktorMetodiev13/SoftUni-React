ff5d6053e5958b77ea1e7d9cafe22c7a41b19973
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-03-04 20:15:31 +0200

    Simple square area exercise

--- 04. Components (Basic Idea)/react-calculator/test.js
+++ 04. Components (Basic Idea)/react-calculator/test.js
@@ -1,5 +1,7 @@
-function start() {
-    console.log('hello softuni');
-} 
+function start(input) {
+  let squareArea = Number(input[0]);
+  let area = squareArea * squareArea;
+  console.log(area);
+}
 
-start();
+start(["2"]);
