b7bf39a2e32eb27d9d2b34b5ef4d5c78b522725e
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-03-18 17:40:32 +0200

    UserCreate component added

--- 06. Workshop-Components/client/react-user-list-app/src/App.js
+++ 06. Workshop-Components/client/react-user-list-app/src/App.js
@@ -33,9 +33,6 @@ function App() {
 
           <UserList users={users} />
 
-          {/* <!-- New user button  --> */}
-          <button className="btn-add btn">Add new user</button>
-
           {/* <!-- Pagination component  --> */}
           <div className="pagination position">
             <div className="limits">
