7106915fb9ab6b8dd0d172286d0e0da63543a13f
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-03-31 23:56:12 +0300

    changes

--- 06. Workshop-Components/client/react-user-list-app/src/App.js
+++ 06. Workshop-Components/client/react-user-list-app/src/App.js
@@ -37,12 +37,12 @@ function App() {
     const onUserUpdateSubmit = async (e, userId) => {
         e.preventDefault();
 
-        const formData = new FormData(e.currentTarget);
-        const data = Object.fromEntries(formData);
+        // const formData = new FormData(e.currentTarget);
+        // const data = Object.fromEntries(formData);
 
-        const updatedUser = await userService.update(userId, data);
+        // const updatedUser = await userService.update(userId, data);
 
-        setUsers(state => state.map(x => x._id === userId ? updatedUser : x));
+        // setUsers(state => state.map(x => x._id === userId ? updatedUser : x));
     }
 
     const onUserDelete = async (userId) => {
