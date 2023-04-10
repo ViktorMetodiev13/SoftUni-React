3df50a8fdba211dfc4df44ec4387a27bb4e0eb5b
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-04-11 00:46:04 +0300

    React-Router-Dom installed and routes implemented

--- 09. Workshop Routing/games-play/src/App.js
+++ 09. Workshop Routing/games-play/src/App.js
@@ -1,9 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+
 import { Header } from "./components/Header/Header";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+import { Home } from "./components/Home/Home";
+import { CreateGame } from "./components/CreateGame/CreateGame";
+import { Catalog } from "./components/Catalog/Catalog";
 
 function App() {
     return (
         <div id="box">
             <Header />
+
+            <main id="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/catalog" element={<Catalog />} />
+                    <Route path="/create-game" element={<CreateGame />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </main>
         </div>
     );
 }
