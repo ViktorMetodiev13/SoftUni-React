942d0b151cc0fc2298bb6a056baea4d8f1d3ccc0
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2023-04-11 21:48:45 +0300

    GameService created, requester.js, catalog page started

--- 09. Workshop Routing/games-play/src/App.js
+++ 09. Workshop Routing/games-play/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { Header } from "./components/Header/Header";
@@ -7,7 +8,18 @@ import { Home } from "./components/Home/Home";
 import { CreateGame } from "./components/CreateGame/CreateGame";
 import { Catalog } from "./components/Catalog/Catalog";
 
+import * as gameService from "./services/gameService";
+
 function App() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => {
+                setGames(result);
+            })
+    }, []);
+
     return (
         <div id="box">
             <Header />
@@ -15,7 +27,7 @@ function App() {
             <main id="main-content">
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/catalog" element={<Catalog />} />
+                    <Route path="/catalog" element={<Catalog games={games} />} />
                     <Route path="/create-game" element={<CreateGame />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
---
+++ 09. Workshop Routing/games-play/src/services/gameService.js
@@ -0,0 +1,10 @@
+import { request } from "./requester"
+
+const baseUrl = 'http://localhost:3030/jsonstore/games'
+
+export const getAll = async () => {
+    const result = await request('GET', baseUrl);
+    const games = Object.values(result);
+    
+    return games;
+}
