ea088f1850ae7fa678d1567bb8370a803e8f4f1e
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2024-02-02 23:37:13 +0200

    react demo

---
+++ 05. Components Deep Dive/demo/my-app/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return <div>
+    <h1>Hello from React!</h1>
+    <ul>
+      <li>first</li>
+      <li>second</li>
+      <li>third</li>
+    </ul>
+  </div>;
+}
+
+export default App;
