61150fbceebe47b57aafd69910d720e61e372257
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2024-03-11 12:49:25 +0200

    Search, userlist and pagination components added

--- 06. Workshop-Components/09.03.2024 - second attempt/client/src/App.js
+++ 06. Workshop-Components/09.03.2024 - second attempt/client/src/App.js
@@ -1,11 +1,25 @@
 import { Header } from "./components/Header.js";
 import { Footer } from "./components/Footer.js";
+import { Search } from "./components/Search.js";
+import "./App.css";
+import { UserList } from "./components/Userlist.js";
+import { Pagination } from "./components/Pagination.js";
 
 function App() {
   return (
     <>
-        <Header />
-        <Footer />
+      <Header />
+
+      <main className="main">
+        <section className="card users-container">
+          <Search />
+          <UserList />
+          <button className="btn-add btn">Add new user</button>
+          <Pagination />
+        </section>
+      </main>
+
+      <Footer />
     </>
   );
 }
