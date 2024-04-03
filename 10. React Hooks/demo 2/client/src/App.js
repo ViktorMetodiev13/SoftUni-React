98757a8e059964b92fea1a058b8fbadf39248791
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2024-04-03 21:40:00 +0300

    react-bootstrap

--- 10. React Hooks/demo 2/client/src/App.js
+++ 10. React Hooks/demo 2/client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Stack, Button } from "react-bootstrap";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Stack direction="horizontal" gap={2}>
+            <Button as="a" variant="primary">
+                Button as link
+            </Button>
+            <Button as="a" variant="success">
+                Button as link
+            </Button>
+        </Stack>
+    );
 }
 
 export default App;
